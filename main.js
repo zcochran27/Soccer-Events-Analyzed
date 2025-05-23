1945c169045c5963cf1dce8e1b1bb7b630ebe74d
Zach Cochran <zcochran@ucsd.edu>
2025-05-23 12:45:45 -0700

    updated fetch for api

--- main.js
+++ main.js
@@ -312,7 +312,7 @@ const formatted = collectedStats.flatMap(pass => [
 
 
   try {
-    const response = await fetch("http://127.0.0.1:8000/predict", {
+    const response = await fetch("https://soccer-events-analyzed.onrender.com/predict", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
